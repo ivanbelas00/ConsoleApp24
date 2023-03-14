d5ece7af5d219421130b803ebfadc8a6ea42af75
a <a>
2023-03-14 16:14:07 +0100

    Promjena

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp24
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Novo");
         }
     }
 }
