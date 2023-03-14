c4830384cc5a46ff3f12d576d5da6d7ea8759e5b
petraC00 <117638821+petraC00@users.noreply.github.com>
2023-03-14 16:19:46 +0100

    Update Program.cs

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp24
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("Novo");
-            Console.WriteLine("perina promjena");
+            Console.WriteLine("petrina promjena");
         }
     }
 }
